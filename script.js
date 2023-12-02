76558f89adb60a639954f736f86667c50a58b71e
Travis Lamberte <travis@lamberte.com>
2023-12-02 13:06:00 -0800

    add deltaTime which is the frame time in ms

--- script.js
+++ script.js
@@ -89,14 +89,18 @@ window.addEventListener('load', function(){
     }
 
     const game = new Game(canvas.width, canvas.height)
+    let lastTime = 0
     //animation loop
-    function animate(){
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime
+        console.log(deltaTime)
+        lastTime = timeStamp
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         game.update()
         game.draw(ctx)
         requestAnimationFrame(animate)
     }
-    animate()
+    animate(0)
 
 
 
